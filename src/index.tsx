470fb04b7d36ef8140ccdaf1c09e38a1b1c1f89d
Quentin Dejean <quentin.dejean@amaysim.com.au>
2020-03-26 18:37:05 +1100

    task(testing-library): Add react testing library

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
-import purple from '@material-ui/core/colors/purple'
+import { ThemeProvider } from '@material-ui/core/styles'
+
+import theme from 'utils/theme'
 
 import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-
-const theme = createMuiTheme({
-  palette: {
-    primary: purple,
-    secondary: {
-      main: '#f44336',
-    },
-  },
-})
+import App from './app/App'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -24,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
